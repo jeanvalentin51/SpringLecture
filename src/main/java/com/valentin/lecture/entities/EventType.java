4617a0e6e36fd3047982ebe85382c48d47fc612d
jeanvalentin51 <valentin.j.gheorghe@gmail.com>
2021-05-03 18:58:06 -0400

    Update application yml and data sql

--- src/main/java/com/valentin/lecture/entities/EventType.java
+++ src/main/java/com/valentin/lecture/entities/EventType.java
@@ -1,2 +1,27 @@
-package com.valentin.lecture.entities;public class EventType {
+package com.valentin.lecture.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "event_type")
+public class EventType {
+
+    @Id
+    @Column(name = "type_id")
+    private int typeId;
+
+    @Column(name = "event_type", nullable = false, length = 100)
+    private String eventType;
+
+/*
+
+Event types:
+
+    0 - Milonga
+    1 - Workshop
+    2 - Festival
+    3 - Class
+*/
+
+
 }
